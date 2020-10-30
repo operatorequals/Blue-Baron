5dbc59a8227b444413787d414f206e244b360a29
John Torakis <john.torakis@gmail.com>
2020-10-31 00:46:50 +0200

    Ingester integration with MongoDB in Minikube
    The Fluentd ingester is used to couple the MongoDB Data Lake
    with Elasticsearch in the Minikube example deployment.
    
    For this to happen, a custom docker image was built,
    based on `gcr.io/google-containers/fluentd-elasticsearch:v2.4.0`
    that contains a build of the native `fluent-plugin-mongo` [1]
    
    [1] :https://github.com/fluent/fluent-plugin-mongo/issues/149

--- examples/minikube/ingester.tf
+++ examples/minikube/ingester.tf
@@ -4,23 +4,27 @@ module "ingester" {
   namespace = "default"
   values-file = "examples/minikube/ingester.values.yaml"
 
-
   es_host = module.elastic-cluster.es-dns-internal
+  es_index = "logstash-mongo"
 
-  es_index = "logstash-test"
-
-  es_credentials_k8s_secret = module.elastic-cluster.es-user-secret
-
-  es_tls_k8s_secret = module.elastic-cluster.es-certs
+  log_source = "test"
 
   es_username = "elastic"
+  es_credentials_k8s_secret = module.elastic-cluster.es-user-secret
+  es_tls_k8s_secret = module.elastic-cluster.es-certs
 
-  # https://github.com/nttcom/fluent-plugin-rabbitmq
-  # fluentd_plugin_list = ["--ignore-dependencies fluent-plugin-mongo"]
+
+  # The "fluent-plugin-mongo" plugin is already installed in
+  # the used docker image "operatorequals/fluentd-elasticsearch:2.4.0-mongo"
+  # 
+  #   https://github.com/fluent/fluent-plugin-mongo/issues/149
   fluentd_plugin_list = []
 
-  ### HARDCODED ALL
-  # fluentd_configs = {}
+  environment = {
+    MONGODB_USERNAME = "baron",
+    MONGODB_PASSWORD = data.kubernetes_secret.mongodb.data.mongodb-password,
+  }
+
   fluentd_configs = {
     mongo-source = <<EOF
 <source>
@@ -28,10 +32,11 @@ module "ingester" {
 
   database fluent
   collection capped_log
-  host mongodb.default
+  host "${module.mongodb.mongodb-svc-dns}"
+  port 27017
 
-  user baron
-  password "${data.kubernetes_secret.mongodb.data.mongodb-password}"
+  user     "#{ENV['MONGODB_USERNAME']}"
+  password "#{ENV['MONGODB_PASSWORD']}"
 
   tag app.mongo_log
 
@@ -44,9 +49,7 @@ module "ingester" {
   # Convert ObjectId to string
   object_id_keys ["id_key"]
 </source>
-
 EOF
   }
 
-  log_source = "test"
 }
