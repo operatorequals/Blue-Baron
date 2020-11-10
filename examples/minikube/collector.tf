d86b79f25da6fe53b88bff76ea3bc91573bb6652
gian <gian2dchris@gmail.com>
2020-11-10 17:10:42 +0200

    Sanitize Collector keys, that break MongoDB
    This commit sanitizes invalid JSON keys,
    which result in corrupted Mongo entries.
    
    Specifically keys that start with `$` or include `.`.
    
    It does so by replacing the invalid characters with
    `__dollar__` or `__dot__`, correspondingly, leveraging
    fluent-mongo-plugin replace feature[1].
    
    [1] https://github.com/fluent/fluent-plugin-mongo#replace_dot_in_key_with-and-replace_dollar_in_key_with-

--- examples/minikube/collector.tf
+++ examples/minikube/collector.tf
@@ -9,8 +9,31 @@ module "collector" {
     MONGODB_PASSWORD = data.kubernetes_secret.mongodb.data.mongodb-password,
   }
 
+  fluentd_plugin_list = [
+    "fluent-plugin-beats",
+    "fluent-plugin-rename-key"
+    ]
+
   fluentd_configs = {
     mongo-match = <<EOF
+<filter **>
+  @type rename_key
+
+  # BSON records which include '.' or start with '$' are invalid 
+  # and they will be stored as broken data to MongoDB.
+  # Note: This rule replaces '$' only on first characters with 'x$'.
+  
+  rename_rule1 ^\\$(.+) __dollar__$${md[1]}
+  rename_rule2 ^\\.(.+) __dot__$${md[1]}
+</filter>
+<filter **>
+  @type rename_key
+  rename_rule1 (.+)\\.(.+) $${md[1]}__dot__$${md[2]}
+</filter>
+<filter **>
+  @type rename_key
+  rename_rule1 (.+)\\.(.+) $${md[1]}__dot__$${md[2]}
+</filter>
 <match **>
   @type mongo
 
@@ -25,6 +48,9 @@ module "collector" {
   
   capped
   capped_size 100m
+
+  # replace_dot_in_key_with __dot__
+  # replace_dollar_in_key_with __dollar__
 
 </match>
 EOF
