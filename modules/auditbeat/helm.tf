26b99fb704eabd726671101624430f141f83183a
gian <gian2dchris@gmail.com>
2020-12-02 15:36:30 +0200

    Enable Kibana dashboards Auditbeat TF Module

--- modules/auditbeat/helm.tf
+++ modules/auditbeat/helm.tf
@@ -15,5 +15,29 @@ resource "helm_release" "auditbeat" {
     name = "config.output\\.logstash.hosts[0]"
     value = var.collector-url
   }
+  
+  set {
+    name = "config.setup\\.kibana.username"
+    value = var.es_username
+  }
 
+  set_sensitive {
+    name = "config.setup\\.kibana.password"
+    value = data.kubernetes_secret.es_credentials_k8s_secret.data[var.es_username]
+  }
+
+  set {
+    name = "config.setup\\.kibana.host"
+    value = var.kibana-host
+  }
+  
+  set {
+    name  = "extraVolumes[0].secret.secretName"
+    value = var.kb_tls_k8s_secret
+  }
+
+  set {
+    name = "config.setup.dashboards.enabled"
+    value = var.enable_dashboards
+  }
 }
--- modules/auditbeat/variables.tf
+++ modules/auditbeat/variables.tf
@@ -5,6 +5,27 @@ variable "values-file" {
 
 variable collector-url {}
 
+variable kibana-host {
+  default = ""
+}
+
+variable es_username {
+  default = ""
+}
+
+variable es_credentials_k8s_secret {
+  default = ""
+}
+
+variable kb_tls_k8s_secret {
+  default = ""
+}
+
+variable enable_dashboards {
+  type = bool
+  default = false
+}
+
 variable namespace {
     default = "audit"
 }
