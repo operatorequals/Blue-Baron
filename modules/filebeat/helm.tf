b23556f654bcd502ae7cfc0e9a32a70a9ddf6329
gian <gian2dchris@gmail.com>
2020-12-03 12:01:35 +0200

    Enable Kibana dashboards Filebeat TF Module

--- modules/filebeat/helm.tf
+++ modules/filebeat/helm.tf
@@ -2,7 +2,7 @@
 resource "helm_release" "filebeat" {
   name       = "filebeat"
   chart      = "filebeat"
-  repository = "https://helm.elastic.co"
+  repository = "https://helm.elastic.co/"
   namespace  = var.namespace
   version    = "7.9.0"
   create_namespace = true
@@ -19,6 +19,57 @@ resource "helm_release" "filebeat" {
   set {
     name = "extraEnvs[0].value"
     value = var.collector-url
+  }
+  
+  set {
+    name = "extraEnvs[1].name"
+    value = "ES_USERNAME"
+  }
+
+  set_sensitive {
+    name = "extraEnvs[1].value"
+    value = var.es_username
+  }
+
+  set {
+    name = "extraEnvs[2].name"
+    value = "KB_HOSTNAME"
+  }
+
+  set_sensitive {
+    name = "extraEnvs[2].value"
+    value = var.kibana-host
+  }
+  
+  set {
+    name = "extraEnvs[3].name"
+    value = "ES_PASSWORD"
+  }
+
+  set_sensitive {
+    name = "extraEnvs[3].valueFrom.secretKeyRef.name"
+    value = var.es_credentials_k8s_secret
+  }
+  
+  set_sensitive {
+    name = "extraEnvs[3].valueFrom.secretKeyRef.key"
+    value = var.es_username
+  }
+
+  # Load Kibana Dashboards Switch 
+  # set {
+  #   name = "extraEnvs[4].name"
+  #   value = "DASHBOARDS"
+  # }
+
+  # set {
+  #   name = "extraEnvs[4].value"
+  #   value = var.enable_dashboards
+  # }
+
+  set {
+    name = "extraVolumes[0].secret.secretName"
+    value = var.kb_tls_k8s_secret
   }
 
 }
--- modules/filebeat/variables.tf
+++ modules/filebeat/variables.tf
@@ -5,6 +5,27 @@ variable "values-file" {
 
 variable collector-url {}
 
+# Enable Dashboards Switch
+# variable enable_dashboards {
+#   default = "false"
+# }
+
+variable kibana-host {
+  default = ""
+}
+
+variable es_username {
+  default = ""
+}
+
+variable es_credentials_k8s_secret {
+  default = ""
+}
+
+variable kb_tls_k8s_secret {
+  default = ""
+}
+
 variable namespace {
     default = "audit"
 }
