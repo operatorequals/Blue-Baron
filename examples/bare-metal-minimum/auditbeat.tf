4a0359a7389bf5224849df76f394f175cb886c54
gian <gian2dchris@gmail.com>
2020-12-04 17:30:13 +0200

    Deploy Auditbeat with Kibana Dashboards for Bare-Metal examples

--- examples/bare-metal-minimum/auditbeat.tf
+++ examples/bare-metal-minimum/auditbeat.tf
@@ -4,4 +4,11 @@ module "auditbeat" {
   namespace = var.audit-ns
 
   collector-url = module.collector.collector_beats_url
+  enable_dashboards = true
+  kibana-host = module.elastic-cluster.kb-url-internal
+  es_username = "elastic"
+  es_credentials_k8s_secret = module.elastic-cluster.es-user-secret
+  kb_tls_k8s_secret = module.elastic-cluster.kb-certs
+
+  depends_on = [module.elastic-cluster]
 }
