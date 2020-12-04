0d3e522100eeffacc43ee836328e20c4bd72d836
gian <gian2dchris@gmail.com>
2020-12-04 17:30:52 +0200

    Deploy Filebeat with Kibana Dashboards for Bare-Metal examples

--- examples/bare-metal/filebeat.tf
+++ examples/bare-metal/filebeat.tf
@@ -4,4 +4,10 @@ module "filebeat" {
   namespace = var.audit-ns
 
   collector-url = module.collector.collector_beats_url
+  kibana-host = module.elastic-cluster.kb-dns-internal
+  es_username = "elastic"
+  es_credentials_k8s_secret = module.elastic-cluster.es-user-secret
+  kb_tls_k8s_secret = module.elastic-cluster.kb-certs
+
+  depends_on = [module.elastic-cluster]
 }
