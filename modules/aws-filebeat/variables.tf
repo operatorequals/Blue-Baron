7b8993865696157bc7eb1db259607aeb67d13235
John Torakis <john.torakis@gmail.com>
2020-12-10 02:09:53 +0200

    Filebeat TF Module that accepts IAM Keys and Pod Annotations
    A Filebeat Terraform Module is introduced that accepts all
    Elasticsearch and Kibana URLs, credentials and TLS secrets
    along with an AWS Log Source (e.g CloudTrail, EC2, ELB, etc)
    and ingests the logs into the cluster.
    
    Specifically, to authenticate to AWS services, it can use
    either AWS User `ACCESS_KEY_ID`/`SECRET_ACCESS_KEY` for on-premise
    Kubernetes clusters (outside AWS) or Kubernetes Pod annotations
    for use with `kube2iam` [1] or `kiam` [2].
    
    [1] :https://github.com/jtblin/kube2iam
    [2] :https://github.com/uswitch/kiam

---
+++ modules/aws-filebeat/variables.tf
@@ -0,0 +1,52 @@
+variable namespace {
+}
+
+variable pod_annotations {
+  description = "A Map of annotations to assign to the created Kubernetes Pods (for IAM Role assignment)"
+  default = {}
+}
+
+variable "values-file" {
+  description = "The 'values.yaml' file location for the 'aws-filebeat' helm chart"
+  default = "charts/aws-filebeat/values.yaml"
+}
+
+variable aws_sqs_queue_url{
+  description = "The URL of the AWS SQS queue that can be polled for new log files"
+}
+
+variable aws_log_source {
+  description = "Filebeat AWS module type (\"elb\", \"cloudtrail\", etc)"
+}
+
+variable aws_iam_user_access_key_id {
+  default = ""
+}
+
+variable aws_iam_user_secret_access_key {
+  default = ""
+}
+
+variable kb_host {
+  description = "The Kibana instance URL to setup Index Patterns and Dashboards"
+}
+
+variable es_host {
+  description = "The Elasticsearch instance URL to push the logs to"
+}
+
+variable es_credentials_k8s_secret {
+  description = "Kubernetes Secret name created by ECK Helm chart - contains Elasticsearch users"
+}
+
+variable es_tls_k8s_secret {
+  description = "Kubernetes Secret name created by ECK Helm chart - contains Elasticsearch TLS CA certificate"
+}
+
+variable kb_tls_k8s_secret {
+  description = "Kubernetes Secret name created by ECK Helm chart - contains Kibana TLS CA certificate"
+}
+
+variable es_username {
+    default = "elastic"
+}
