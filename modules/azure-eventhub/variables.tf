772327b9fe2751b2535c35f64a39c2cab4f5797e
gian <gian2dchris@gmail.com>
2020-12-27 03:29:13 +0200

    Azure Blue Baron Eventhub Namespace TF Module
    This commit creates an Azure Eventhub Namespace TF Module.
    The Eventhub Namespace is essentially the Eventhub Cluster or Broker
    where Azure logs are exported (each in a different Eventhub),
    in order to be ingested by external systems (e.g. Filebeat).

---
+++ modules/azure-eventhub/variables.tf
@@ -0,0 +1,15 @@
+variable resource_group_name {
+    default = "bluebaronrg"
+}
+
+variable location {
+    default = "westeurope"
+}
+
+variable eventhub_namespace_name {
+    default = "blueBaronEventHubNamespace"
+}
+
+variable eventhub_name {
+    default = "insights-activity-logs"
+}
